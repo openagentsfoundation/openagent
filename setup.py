614c603df05a2687731d7a65050d92f741f9ed5e
Zhiqiang <494146653@qq.com>
2025-05-28 11:28:11 +0800

    bump verion to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='openagents-sdk',                 
-    version='0.0.1',              
+    version='0.0.2',              
     description='A test package', 
     author='Your Name',
     packages=find_packages(),
